cd921ea215a3bd95ee67e3bf163b78b3c73a1700
washingtron <twashington@vividcloud.com>
2025-01-26 21:03:40 -0500

    spiraled

--- twashington/spiral.py
+++ twashington/spiral.py
@@ -70,39 +70,46 @@ def spiralify(input_list):
 
 
 def spiralify2(raw):
-    x0, x1, y0, y1 = 0, len(raw[0]), 0, len(raw)
+    x0, x1, y0, y1 = 0, len(raw[0]) - 1, 0, len(raw) - 1
     output = []
-    while x0 < x1 and y0 < y1:
+    while x0 <= x1 and y0 <= y1:
         if x0 == x1:
-            output.append([raw[x0, a] for a in range(y0, y1 + 1)])
+            output.append([raw[a][x0] for a in range(y0, y1 + 1)])
         elif x0 + 1 == x1:
             output.append(
-                [raw[x0, y0]] + [raw[x1, a] for a in range(y0, y1 + 1)] + [raw[x0, a] for a in
-                                                                           range(y1, y0 - 1, -1)])
+                [raw[y0][x0]] + [raw[a][x1] for a in range(y0, y1 + 1)] + [raw[a][x0] for a in
+                                                                           range(y1, y0, -1)])
         elif y0 == y1:
-            output.append([raw[a, y0] for a in range(x0, x1 + 1)])
+            output.append([raw[y0][a] for a in range(x0, x1 + 1)])
         elif y0 + 1 == y1:
             output.append(
-                [raw[a, y0] for a in range(x0, x1 + 1)] + [raw[a, y1] for a in range(x1, x0 - 1, -1)])
+                [raw[y0][a] for a in range(x0, x1 + 1)] + [raw[y1][a] for a in range(x1, x0 - 1, -1)])
         else:
-            output.append([raw[a, y0] for a in range(x0, x1 + 1)])
-            output.append([raw[x1, a] for a in range(y0, y1 + 1)])
-            output.append([raw[a, y1] for a in range(y1, y0 - 1, -1)])
-            output.append([raw[x0, a] for a in range(x1, x0 - 1, -1)])
+            output.append([raw[y0][a] for a in range(x0, x1 + 1)])
+            output.append([raw[a][x1] for a in range(y0 + 1, y1 + 1)])
+            output.append([raw[y1][a] for a in range(x1 - 1, x0 - 1, -1)])
+            output.append([raw[a][x0] for a in range(y1 - 1, y0, -1)])
         x0 = x0 + 1
         x1 = x1 - 1
         y0 = y0 + 1
         y1 = y1 - 1
-    return output
+    return [b for item in output for b in item]  # output
 
 
 def main():
     input_matrix_list = get_input_list("matrix_input.txt")
+    input_matrix_list.append([[i * 26 + j + 1 for j in range(26)] for i in range(23)])
+    input_matrix_list.append([[i * 35 + j + 1 for j in range(35)] for i in range(23)])
+    input_matrix_list.append([[i * 35 + j + 1 for j in range(35)] for i in range(44)])
     start = time.perf_counter_ns()
     for input_list in input_matrix_list[2:]:
         output_list = spiralify2(input_list)
         end = time.perf_counter_ns()
         print(f"Input: {input_list}, output: {output_list}")
+        if len(output_list) != len([b for item in input_list for b in item]):
+            print(f'Error, input and output lists have different number of elements')
+        if len(set(output_list)) != len(output_list):
+            print(f'Error, output list values are not unique. len(set(output_list)) ={len(set(output_list))}, len(output_list) = {len(output_list)}')
     print(f"Time taken: {(end - start):.4f} ns")
 
 
